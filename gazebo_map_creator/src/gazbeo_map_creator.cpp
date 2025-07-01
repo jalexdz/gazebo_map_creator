7914bb83620b14410fc84e35250c578847bc306e
Alex Diaz <j.a.diaz@utexas.edu>
2025-07-01 09:37:56 -0600

    Remove unused map functionality

--- gazebo_map_creator/src/gazbeo_map_creator.cpp
+++ gazebo_map_creator/src/gazbeo_map_creator.cpp
@@ -29,7 +29,7 @@
 #include <boost/shared_ptr.hpp>
 #include <pcl/io/pcd_io.h>
 #include <pcl/point_types.h>
-#include <octomap/octomap.h>
+// #include <octomap/octomap.h>
 
 namespace gazebo
 {
@@ -71,208 +71,202 @@ class GazeboMapCreator : public gazebo::SystemPlugin
         std::placeholders::_1, std::placeholders::_2));
   }
   
-  public: void OnMapCreate(const std::shared_ptr<gazebo_map_creator_interface::srv::MapRequest::Request> _req,
-                      std::shared_ptr<gazebo_map_creator_interface::srv::MapRequest::Response>  _res )
+  public: void DisableRobotCollisions(const std::string& model_name)
   {
-    RCLCPP_INFO(ros_node_->get_logger(), "Received message");
-
-     // Calculate the size of the cubic area
-    float size_x = _req->upperleft.x - _req->lowerright.x;
-    float size_y = _req->lowerright.y - _req->upperleft.y;  // size_y to be -ve
-    float size_z = _req->upperleft.z - _req->lowerright.z;
-    
-    // Check for coordinate validity
-    if (size_x <= 0 || size_y >=0 || size_z <= 0 )
+    physics::ModelPtr model = world_->ModelByName(model_name);
+    if (!model)
     {
-        RCLCPP_ERROR(ros_node_->get_logger(), "Invalid coordinates");
-        _res->success = false;
+        std::cout << "Model " << model_name << " not found in the world!" << std::endl;
         return;
     }
-    // Calculate the number of points in each dimension
-    int num_points_x = static_cast<int>(std::abs(size_x) / _req->resolution) + 1;
-    int num_points_y = static_cast<int>(std::abs(size_y) / _req->resolution) + 1;
-    int num_points_z = static_cast<int>(std::abs(size_z) / _req->resolution) + 1;
-    
-    // Calculate the step size in each dimension
-    float step_x = size_x / num_points_x;
-    float step_y = size_y / num_points_y;
-    float step_z = size_z / num_points_z;   
- 
-    int dims = 6;
 
-    if (_req->skip_vertical_scan) 
+    std::cout << "Disabling collisions for model: " << model_name << std::endl;
+
+    for (auto link : model->GetLinks())
     {
-      num_points_z = 2;
-      step_z = size_z;
-      dims = 4;
-    }
-
-    std::cout << "-----------------" << std::endl << "Area Corners: (lower right, upper left)  (" <<
-      _req->lowerright.x << ", " << _req->lowerright.y << ", " << _req->lowerright.z << "), (" <<
-      _req->upperleft.x << ", " << _req->upperleft.y << ", " << _req->upperleft.z << ") " <<  std::endl <<
-      "Area size : " << size_x << " x " << size_y << " x " << size_z << " (WxLxH)" << std::endl <<
-      "Step size : " << step_x << ", " << step_y << ", " << step_z << " (stepx, stepy, stepz) " << std::endl <<
-      "Resolution: (" << num_points_x << ", " <<  num_points_y << ", " << num_points_z << ") - "  << _req->resolution << std::endl <<
-      "Map Mode: " << (_req->skip_vertical_scan ? "Partial Scan": "Full Scan")  << std::endl << "-----------------" << std::endl ;
-
-    // Create ray object for collision detection
-    gazebo::physics::PhysicsEnginePtr engine = world_->Physics();
-    engine->InitForThread();
-    gazebo::physics::RayShapePtr ray =
-        boost::dynamic_pointer_cast<gazebo::physics::RayShape>(
-        engine->CreateShape("ray", gazebo::physics::CollisionPtr()));
-
-    // Pixed values near to 0 means occupied and 255 means empty
-    boost::gil::gray8_pixel_t fill(255 - _req->threshold_2d);
-    boost::gil::gray8_pixel_t blank(255);
-    boost::gil::gray8_image_t image(num_points_x, num_points_y);
-
-    // Initially fill all area with empty pixel value
-    boost::gil::fill_pixels(image._view, blank);
-
-    // Create a point cloud object
-    pcl::PointCloud<pcl::PointXYZ> cloud;
-    std::string entityName;
-    ignition::math::Vector3d start, end;
-    double dx, dy, dz, dist;
-    cloud.width    = num_points_x;
-    cloud.height   = num_points_y;
-        
-    struct PointMask {
-                int x, y, z;
-            };
-    PointMask directions[6] = {
-                {-1, 0, 0}, // Left
-                {1, 0, 0},  // Right
-                {0, 1, 0},  // Front
-                {0, -1, 0}, // Back
-                {0, 0, 1},  // Top
-                {0, 0, -1}  // Bottom
-            };
-
-    for (int x = 0; x < num_points_x; ++x) 
-    {
-        std::cout << "\rPercent complete: " << x * 100.0 / num_points_x << "%       " << std::flush;
-        
-        double cur_x = _req->lowerright.x + x * step_x; 
-        for (int y = 0; y < num_points_y; ++y)
+        for (auto collision : link->GetCollisions())
         {
-            double cur_y = _req->upperleft.y + y * step_y;    
-            
-            ignition::math::Vector3d startV(cur_x, cur_y, _req->lowerright.z);
-            ignition::math::Vector3d endV(cur_x, cur_y, _req->upperleft.z);
-            
-            // Detect collision upward until top of area.  z direction
-            ray->SetPoints(startV, endV);
-            ray->GetIntersection(dist, entityName);              
-            if (!entityName.empty())
-            { 
-              image._view(x,y) = fill;                      
-            }
-           
-
-            // Walk in z direction to check each point for any collision to it's neighbours
-            for (int z = 0; z < num_points_z; ++z)
-            {
-                double cur_z = _req->lowerright.z + z * step_z;
-                ignition::math::Vector3d start(cur_x, cur_y, cur_z);
-
-                // Check for right, left, front, back, top and bottom points for collision.
-                // 2d mode checks for right, left, front and back.  see dims assignment.
-                for(int i = 0; i < dims; ++i) {
-                    dx =  directions[i].x * step_x * _req->range_multiplier;
-                    dy =  directions[i].y * step_y * _req->range_multiplier;
-                    dz =  directions[i].z * step_z *_req->range_multiplier;
-                    ignition::math::Vector3d end(cur_x + dx, cur_y + dy, cur_z + dz);
-                    
-                    ray->SetPoints(start, end);
-                    ray->GetIntersection(dist, entityName);
-                    if (!entityName.empty())
-                    { 
-                      // Collision found.  Push point to cloud and set in image
-                      cloud.push_back(pcl::PointXYZ(cur_x, cur_y, cur_z));
-                      image._view(x,y) = fill;
-                      break;
-                    }            
-                }
-            }
+            collision->SetCollideBits(0);
+            collision->SetCategoryBits(0);
         }
     }
-   
-    std::cout << std::endl << "Completed calculations, writing to image" << std::endl;
-    
-    if (!_req->filename.empty())
-    { 
-        if(cloud.size() > 0)
-        {
-          // Save pcd file
-          pcl::io::savePCDFileASCII (_req->filename + ".pcd", cloud);
-        
-          // Save octomap file
-          octomap::OcTree octree(_req->resolution);
-          for (auto p:cloud.points)
-              octree.updateNode(octomap::point3d(p.x, p.y, p.z), true );
-          octree.updateInnerOccupancy();
-          octree.writeBinary(_req->filename + ".bt");
-        }
-
-        // Save png file
-        boost::gil::gray8_view_t view = image._view;
-        boost::gil::png_write_view(_req->filename+".png", view); 
-
-        // Save pgm file
-        pgm_write_view(_req->filename, view);
-
-        // Write down yaml file for nav2 usage.
-        std::unordered_map<std::string, std::string> yaml_dict;
-        yaml_dict["image"] = _req->filename + ".pgm";
-        yaml_dict["mode"] = "trinary";
-        yaml_dict["resolution"] = std::to_string(_req->resolution);
-        yaml_dict["origin"] =  "[" + std::to_string(_req->lowerright.x) + std::string(", ") + std::to_string(_req->lowerright.y) + std::string(", 0.0]");
-        yaml_dict["negate"] = "0";
-        yaml_dict["occupied_thresh"] = "0.95";  // hardcoding these values since we absolutely know occupied or free
-        yaml_dict["free_thresh"] = "0.90";  
-
-        std::ofstream outputFile(_req->filename + ".yaml");
-        if (outputFile.is_open()) {
-            for (const auto& pair : yaml_dict) {
-                outputFile << pair.first << ": " << pair.second << std::endl;
-            }
-            outputFile.close();
-        } else {
-            std::cout << "Unable to open yaml file for writing." << std::endl;
-        }
-
-        std::cout << "Output location: " << _req->filename + "[.pcd, .bt, .pgm, .png, .yaml]" << std::endl;
-    }
-
-   
-    std::cout << std::endl;
-    _res->success = true;
   }
 
-
-  public: void pgm_write_view(const std::string& filename, boost::gil::gray8_view_t& view)
+  void RestoreRobotCollisions(const std::string& model_name)
   {
-    // Write image to pgm file
-
-    int h = view.height();
-    int w = view.width();
-
-    std::ofstream ofs;
-    ofs.open(filename+".pgm");
-    ofs << "P2" << '\n';          // grayscale
-    ofs << w << ' ' << h << '\n'; // width and height
-    ofs << 255 <<  '\n';          // max value
-    for (int y = 0; y < h; ++y){
-      for (int x = 0; x < w; ++x){
-        // std::cout << (int)view(x, y)[0];
-        ofs << (int)view(x, y)[0] << ' ';
+      physics::ModelPtr model = world_->ModelByName(model_name);
+      if (!model)
+      {
+          std::cout << "Model " << model_name << " not found in the world!" << std::endl;
+          return;
       }
-      ofs << '\n';
-    }
-    ofs.close();
+
+      std::cout << "Restoring collisions for model: " << model_name << std::endl;
+
+      for (auto link : model->GetLinks())
+      {
+          for (auto collision : link->GetCollisions())
+          {
+              collision->SetCollideBits(GZ_ALL_COLLIDE);
+              collision->SetCategoryBits(GZ_ALL_COLLIDE);
+          }
+      }
   }
+
+public: void OnMapCreate(const std::shared_ptr<gazebo_map_creator_interface::srv::MapRequest::Request> _req,
+                         std::shared_ptr<gazebo_map_creator_interface::srv::MapRequest::Response> _res )
+{
+  RCLCPP_INFO(ros_node_->get_logger(), "Received message");
+
+  // Pause simulation so robot doesn't fall
+  world_->SetPaused(true);
+
+  // Disable robot collisions so it doesn't occlude the scan
+  DisableRobotCollisions("velodyne_sensor");
+
+  // Lookup reference frame pose
+  physics::ModelPtr ref_model = world_->ModelByName("velodyne_sensor");
+  physics::LinkPtr ref_link = ref_model->GetLink("base_link");
+  if (!ref_model)
+  {
+      RCLCPP_ERROR(ros_node_->get_logger(), "Reference frame model '%s' not found!", "velodyne_sensor");
+      _res->success = false;
+      world_->SetPaused(false);
+      return;
+  }
+  ignition::math::Pose3d ref_pose = ref_link->WorldPose();
+
+  // Compute bounding box sizes
+  float size_x = _req->upperleft.x - _req->lowerright.x;
+  float size_y = _req->lowerright.y - _req->upperleft.y;  // y negative
+  float size_z = _req->upperleft.z - _req->lowerright.z;
+
+  if (size_x <=0 || size_y >=0 || size_z <=0)
+  {
+      RCLCPP_ERROR(ros_node_->get_logger(), "Invalid coordinates");
+      _res->success = false;
+      world_->SetPaused(false);
+      return;
+  }
+
+  int num_points_x = static_cast<int>(std::abs(size_x) / _req->resolution) + 1;
+  int num_points_y = static_cast<int>(std::abs(size_y) / _req->resolution) + 1;
+  int num_points_z = static_cast<int>(std::abs(size_z) / _req->resolution) + 1;
+
+  float step_x = size_x / num_points_x;
+  float step_y = size_y / num_points_y;
+  float step_z = size_z / num_points_z;
+
+  int dims = 6;
+
+  if (_req->skip_vertical_scan)
+  {
+    num_points_z = 2;
+    step_z = size_z;
+    dims = 4;
+  }
+
+  // Ray setup
+  gazebo::physics::PhysicsEnginePtr engine = world_->Physics();
+  engine->InitForThread();
+  gazebo::physics::RayShapePtr ray =
+      boost::dynamic_pointer_cast<gazebo::physics::RayShape>(
+      engine->CreateShape("ray", gazebo::physics::CollisionPtr()));
+
+  pcl::PointCloud<pcl::PointXYZ> cloud;
+  std::string entityName;
+  ignition::math::Vector3d start, end;
+  double dx, dy, dz, dist;
+  cloud.width = num_points_x;
+  cloud.height = num_points_y;
+
+  struct PointMask {
+      int x, y, z;
+  };
+  PointMask directions[6] = {
+      {-1,0,0},
+      {1,0,0},
+      {0,1,0},
+      {0,-1,0},
+      {0,0,1},
+      {0,0,-1}
+  };
+
+  for (int x =0; x < num_points_x; ++x)
+  {
+    std::cout << "\rPercent complete: " << x * 100.0 / num_points_x << "%    " << std::flush;
+
+    // Coordinates relative to reference frame
+    double local_x = _req->lowerright.x + x * step_x;
+
+    for (int y = 0; y < num_points_y; ++y)
+    {
+      double local_y = _req->upperleft.y + y * step_y;
+
+      for (int z=0; z<num_points_z; ++z)
+      {
+        double local_z = _req->lowerright.z + z * step_z;
+        //ignition::math::Vector3d rel(local_x, local_y, local_z);
+
+        // Transform to world
+        ignition::math::Vector3d cur(local_x, local_y, local_z); //ref_pose.Pos() + ref_pose.Rot().RotateVector(rel);       
+
+        // For each direction
+        for (int i = 0; i < dims; ++i)
+        {
+          dx = directions[i].x * step_x * _req->range_multiplier;
+          dy = directions[i].y * step_y * _req->range_multiplier;
+          dz = directions[i].z * step_z * _req->range_multiplier;
+          ignition::math::Vector3d end_point = cur + ignition::math::Vector3d(dx, dy, dz);
+
+          ray->SetPoints(cur, end_point);
+          ray->GetIntersection(dist, entityName);
+          if (!entityName.empty())
+          {
+            cloud.push_back(pcl::PointXYZ(cur.X(), cur.Y(), cur.Z()));
+            break;
+          }
+        }
+      }
+    }
+  }
+
+  std::cout << std::endl << "Completed calculations, writing to output" << std::endl;
+
+  if (!_req->filename.empty() && cloud.size()>0)
+  {
+      pcl::io::savePCDFileASCII(_req->filename+".pcd", cloud);
+      RCLCPP_INFO(ros_node_->get_logger(), "Saved pointcloud to: %s.pcd", _req->filename.c_str());
+  }
+
+  RestoreRobotCollisions("velodyne_sensor");
+  world_->SetPaused(false);
+  _res->success = true;
+}
+
+
+
+  // public: void pgm_write_view(const std::string& filename, boost::gil::gray8_view_t& view)
+  // {
+  //   // Write image to pgm file
+
+  //   int h = view.height();
+  //   int w = view.width();
+
+  //   std::ofstream ofs;
+  //   ofs.open(filename+".pgm");
+  //   ofs << "P2" << '\n';          // grayscale
+  //   ofs << w << ' ' << h << '\n'; // width and height
+  //   ofs << 255 <<  '\n';          // max value
+  //   for (int y = 0; y < h; ++y){
+  //     for (int x = 0; x < w; ++x){
+  //       // std::cout << (int)view(x, y)[0];
+  //       ofs << (int)view(x, y)[0] << ' ';
+  //     }
+  //     ofs << '\n';
+  //   }
+  //   ofs.close();
+  // }
 };
 
 // Register this plugin with the simulator
